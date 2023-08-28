444e5787032b382cfd7bb2d0eb67617505793366
Dev789-del <huynq.bi11-113@st.usth.edu.vn>
2023-08-28 21:23:23 +0700

    Publish practical work 1-5

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -0,0 +1,54 @@
+package vn.edu.usth.weather;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ForecastFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ForecastFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static ForecastFragment newInstance(String param1, String param2) {
+        ForecastFragment fragment = new ForecastFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View FragmentView = inflater.inflate(R.layout.fragment_forecast, container, false);
+        FragmentView.setBackgroundColor(Color.parseColor("#ffffff"));
+        return FragmentView;
+    }
+}
---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,54 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toolbar;
+
+public class WeatherActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "WeatherActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Create a new Fragment to be placed in the activity layout
+        ForecastFragment firstFragment = new ForecastFragment();
+        // Add the fragment to the 'container' FrameLayout
+        View frameworkLayout = findViewById(R.id.frame_layout);
+        getSupportFragmentManager().beginTransaction().add(R.id.frame_layout, firstFragment).commit();
+        setContentView(R.layout.activity_main);
+        Log.i(LOG_TAG, "onCreate");
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(LOG_TAG, "onPause");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+}
