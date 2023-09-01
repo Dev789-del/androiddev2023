5f79c4ef901385e2be8622132522a9b4cd67554a
Dev789-del <huynq.bi11-113@st.usth.edu.vn>
2023-09-01 09:52:00 +0700

    Publish draft of practical work 6

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
@@ -8,15 +8,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+public class WeatherFragment extends Fragment {
 
-public class ForecastFragment extends Fragment {
+    public WeatherFragment() {
+        // Required empty public constructor
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        View FragmentView = inflater.inflate(R.layout.fragment_forecast, container, false);
-        FragmentView.setBackgroundColor(Color.parseColor("#ffffff"));
+        View FragmentView = inflater.inflate(R.layout.fragment_weather, container, false);
         return FragmentView;
     }
 }
